612732d4c123dc1564390617e2546879715a58e8
RoggJS <dmitry.rogalya@gmail.com>
2020-10-27 16:41:15 +0200

    add slick slider

--- app/js/custom.js
+++ app/js/custom.js
@@ -3,25 +3,15 @@ $(function () {
 
     $('.menu__icon').on('click', function () {
         $(this).closest('.menu__icon').toggleClass('active');
-        $('.header-menu__list').toggleClass('active');
-        $('.header-menu__list').slideToggle('slow', function () {
-            if ($('.header-menu__list').css('display') === 'none') {
-                $('.header-menu__list').removeAttr('style');
+        $('.header-menu').toggleClass('active');
+        $('.header-menu').slideToggle('slow', function () {
+            if ($('.header-menu').css('display') === 'none') {
+                $('.header-menu').removeAttr('style');
             }
         });
     });
 
 
-    $(".submenu-link").on("click", function (event) {
-        //отменяем стандартную обработку нажатия по ссылке
-        event.preventDefault();
-        $('.header-menu__submenu').slideToggle('slow', function () {
-            if ($('.header-menu__submenu').css('display') === 'none') {
-                $('.header-menu__submenu').removeAttr('style');
-            }
-        });
-        $('.submenu-container__arrow').toggleClass('rotate');
-    });
 
     $('#popup-contact-from').on('submit', function () {
 
