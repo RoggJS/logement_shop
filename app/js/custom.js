7c316c4f2f4562090d04fe0ac172f705b257aca8
RoggJS <dmitry.rogalya@gmail.com>
2020-10-27 18:03:34 +0200

    add banner

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,6 +1,55 @@
 $(function () {
 
 
+//1 фото slider
+    $('.header-slider').slick({
+        arrows: true,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
+        responsive: [
+
+            {
+                breakpoint: 1024,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+
+                }
+            },
+            {
+                breakpoint: 960,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    dots: true,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+
+    });
+
+
+
     $('.menu__icon').on('click', function () {
         $(this).closest('.menu__icon').toggleClass('active');
         $('.header-menu').toggleClass('active');
